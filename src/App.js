8a93a224ca3a8ef66f9ff3e1033b2b55d143c8a6
Seweryn Kimla <sewkim@vp.pl>
2018-10-31 17:33:17 +0100

    CRA

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <p>Hello</p>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
